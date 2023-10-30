254f480886e9c8f49ff6e52d337e239937365882
atul-mandackar <atul007atulmandavkar@gmail.com>
2023-10-30 20:20:22 +0530

    Made few corrections in design First is disable the buttons if there is no text in textarea And Second is corrected the seconds require to read text, this correction did with the help of filter fuction which works same as we made function for word count. Also needed more corrections in app

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,16 +37,16 @@ export default function TextForm(props) {
         <div className="mb-3">
           <textarea className={`form-control bg-${props.mode === "light" ? "warning-subtle" : "dark"} p-3 text-start text-${props.mode === "light" ? "dark" : "light"}`} id="exampleFormControlTextarea1" value={text} onChange={handleTextChange} rows="9" style={{backgroundColor: "lightYellow"}}></textarea>
         </div>
-        <button type="button" className={`btn btn-${props.mode === "light" ? "dark" : "light"} mx-2`} onClick={handleUpClick}>Upper Case</button>
-        <button type="button" className={`btn btn-${props.mode === "light" ? "dark" : "light"} mx-2`} onClick={handleLwClick}>Lower Case</button>
-        <button type="button" className={`btn btn-${props.mode === "light" ? "dark" : "light"} mx-2`} onClick={handleClClick}>Clear All</button>
-        <button type="button" className={`btn btn-${props.mode === "light" ? "dark" : "light"} mx-2`} onClick={handleElClick}>Empty Line Remove</button>
+        <button type="button" disabled={text.length===0} className={`btn btn-${props.mode === "light" ? "dark" : "light"} mx-2 my-2`} onClick={handleUpClick}>Upper Case</button>
+        <button type="button" disabled={text.length===0} className={`btn btn-${props.mode === "light" ? "dark" : "light"} mx-2 my-2`} onClick={handleLwClick}>Lower Case</button>
+        <button type="button" disabled={text.length===0} className={`btn btn-${props.mode === "light" ? "dark" : "light"} mx-2 my-2`} onClick={handleClClick}>Clear All</button>
+        <button type="button" disabled={text.length===0} className={`btn btn-${props.mode === "light" ? "dark" : "light"} mx-2 my-2`} onClick={handleElClick}>Empty Line Remove</button>
       </div>
       <div className={`container text-start bg-${props.mode === "light" ? "danger" : "dark-subtle"} bg-opacity-25 text-${props.mode === "light" ? "dark" : "primary"}`}>
         <h3>Your data</h3>
         {/* In next line \s* need to add before \n+ for words count as it count new word for entering in new line and hit space make calculation wrong */}
         <p className='fst-italic fw-semibold'>{text.split("\n").length} lines and {text.trim() === "" ? "0":text.replace(/(\s*\n+\s*|\s+)/g, " ").trim().split(" ").length} words and {text.length} characters</p>
-        <p className='fst-italic fw-semibold'>You can read all in {0.008 * text.split(" ").length} minits</p>
+        <p className='fst-italic fw-semibold'>You can read all in {0.008 * text.split(" ").filter((ele)=>{return ele.length !== 0}).length} minits</p>
         <p>
           <span className='d-block fs-3 fw-bold'>Preview : </span>
           {text.length === 0 ? "Enter some text in textbox for preview" : text}
